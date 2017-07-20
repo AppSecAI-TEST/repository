038ad4c81fd10a49b71a4c6f8586794dff07ef82
zyouke <zyouke@aliyun.com>
2017-07-20 10:31:53 +0800

    data

--- data/src/main/java/com/zyouke/dao/HibernateDao.java
+++ data/src/main/java/com/zyouke/dao/HibernateDao.java
@@ -14,7 +14,7 @@ import org.hibernate.cfg.Configuration;
 
 import com.zyouke.bean.Area;
 
-// 测试使用gitaaaaaaaaaaaaaa
+// 测试使用
 public class HibernateDao {
 
     private static SessionFactory sessionFactory = null;
