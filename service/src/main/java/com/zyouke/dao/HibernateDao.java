5749e643b2ff1f3f2aeb88dd53ed6393ddb3872b
zyouke <zyouke@aliyun.com>
2017-08-10 17:28:03 +0800

    service提交

---
+++ service/src/main/java/com/zyouke/dao/HibernateDao.java
@@ -0,0 +1,130 @@
+package com.zyouke.dao;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.zyouke.bean.Area;
+
+// ����ʹ��
+@Repository("hibernateDao")
+public class HibernateDao {
+
+    @Autowired  
+    private SessionFactory sessionFactory; 
+    
+    
+    public void add(Area area) {
+	Session session = sessionFactory.openSession();
+	Transaction tx = session.beginTransaction();
+	session.save(area);
+	tx.commit();
+	session.close();
+    }
+
+    public void addAll(List<Area> areas) {
+	Session session = sessionFactory.openSession();
+	Transaction tx = session.beginTransaction();
+	for (Area area : areas) {
+	    session.save(area);
+	}
+	session.flush();
+	session.clear();
+	tx.commit();
+	session.close();
+    }
+
+     public synchronized  void addAllSql(List<Area> areas) {
+	System.out.println("----------->" + areas.get(0).getCode());
+	String sqlInsert = "INSERT INTO t_area (CODE,VALUE,PARENT,LEVEL,FULL_NAME) values ";
+	StringBuffer buffer = new StringBuffer(sqlInsert);
+	for (int i = 0; i < areas.size(); i++) {
+	    Area area = areas.get(i);
+	    if (i == areas.size() - 1) {
+		buffer.append("(");
+		buffer.append("'"+area.getCode()+"',");
+		buffer.append("'"+area.getValue()+"',");
+		buffer.append("'"+area.getParent()+"',");
+		buffer.append(area.getLevel()+",");
+		buffer.append("'"+area.getFullName()+"'");
+		buffer.append(");");
+	    } else {
+		buffer.append("(");
+		buffer.append("'"+area.getCode()+"',");
+		buffer.append("'"+area.getValue()+"',");
+		buffer.append("'"+area.getParent()+"',");
+		buffer.append(area.getLevel()+",");
+		buffer.append("'"+area.getFullName()+"'");
+		buffer.append("),");
+	    }
+	}
+	try {
+	    FileWriter fileWritter = new FileWriter("E:/work_doc/demo_file/area_file/t_area.txt", true);
+	    BufferedWriter bufferWritter = new BufferedWriter(fileWritter);
+	    bufferWritter.write(buffer.toString() + "\r\n");
+	    bufferWritter.close();
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
+    }
+
+    public List<Area> queryList() {
+	Session session = sessionFactory.openSession();
+	Transaction tx = session.beginTransaction();
+	String sqlInsert = "select * from t_area where PARENT is null LIMIT 0,5300";
+	Query query = session.createSQLQuery(sqlInsert).addEntity(Area.class);
+	List<Area> list = query.list();
+	tx.commit();
+	session.close();
+	return list;
+    }
+
+    public List<Area> queryList(Map<String, String> map) {
+	Session session = sessionFactory.openSession();
+	Transaction tx = session.beginTransaction();
+	String sqlSelect = "select * from t_area where ";
+	for (String key : map.keySet()) {
+	    sqlSelect = sqlSelect + key + "=" + map.get(key);
+	}
+	Query query = session.createSQLQuery(sqlSelect).addEntity(Area.class);
+	List<Area> list = query.list();
+	tx.commit();
+	session.close();
+	return list;
+    }
+
+    public List<Area> queryListByLimit(int start) {
+	Session session = sessionFactory.openSession();
+	Transaction tx = session.beginTransaction();
+	String sqlSelect = "select * from t_area  LIMIT "+start+",10000";
+	Query query = session.createSQLQuery(sqlSelect).addEntity(Area.class);
+	List<Area> list = query.list();
+	tx.commit();
+	session.close();
+	return list;
+    }
+
+    public synchronized void updateSql(List<Area> areas) {
+	try {
+	    FileWriter fileWritter = new FileWriter("E:/work_doc/demo_file/area_file/update.txt", true);
+	    BufferedWriter bufferWritter = new BufferedWriter(fileWritter);
+	    for (Area area : areas) {
+		String sqlInsert = "UPDATE t_area SET PARENT = " + area.getParent() + ",LEVEL = " + area.getLevel() + ",FULL_NAME = '" + area.getFullName() + "' WHERE ID = " + area.getId();
+		System.out.println("-------->" + area.getId());
+		bufferWritter.write(sqlInsert + ";\r\n");
+	    }
+	    bufferWritter.close();
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
+    }
+}
---
+++ service/src/main/java/com/zyouke/netty/TimeServer.java
@@ -0,0 +1,38 @@
+package com.zyouke.netty;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpRequestDecoder;
+import io.netty.handler.codec.http.HttpResponseEncoder;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
+import com.zyouke.netty.handler.HttpFileServerHandler;
+import com.zyouke.netty.util.NettyUtils;
+
+
+public class TimeServer {
+    
+    public static void main(final String[] args) {
+	//NettyUtils.bind(new TimeServerHandler());
+	/*NettyUtils.bind(new ChannelInitializer<SocketChannel>() {
+		@Override
+		protected void initChannel(SocketChannel sc) throws Exception {
+		    sc.pipeline().addLast(new LineBasedFrameDecoder(1024));
+		    sc.pipeline().addLast(new StringDecoder());
+		    sc.pipeline().addLast(new TimeServerHandlerPackage());
+		}
+	    });*/
+	NettyUtils.bind(new ChannelInitializer<SocketChannel>() {
+	    @Override
+	    protected void initChannel(SocketChannel sc) throws Exception {
+		sc.pipeline().addLast("http_decoder",new HttpRequestDecoder());
+		sc.pipeline().addLast("http_aggregator",new HttpObjectAggregator(65536));
+		sc.pipeline().addLast("http_encoder",new HttpResponseEncoder());
+		sc.pipeline().addLast("http_chunked",new ChunkedWriteHandler());
+		sc.pipeline().addLast("http_file",new HttpFileServerHandler("/usr/"));
+	    }
+	});
+	
+    }
+}
